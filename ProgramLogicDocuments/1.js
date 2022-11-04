d23e85ba5ff703f00ea0e4d8bc903a207000769a
Ihor Butenko <91276970+Ihor-Butenko@users.noreply.github.com>
2022-11-04 23:54:33 +0200

    Add files via upload

---
+++ ProgramLogicDocuments/1.js
@@ -0,0 +1,35 @@
+if (document.getElementById("title")) {
+    document.querySelector(".title-test").style.color = "green"
+    document.querySelector(".title-test").innerHTML = '<p>Title test :: Passed</p><i class="fa-solid fa-circle-check"></i>'
+  } else {
+    document.querySelector(".title-test").style.color = "red"
+  }
+
+if (document.getElementById("paragraph")) {
+    document.querySelector(".paragraph-test").style.color = "green"
+    document.querySelector(".paragraph-test").innerHTML = '<p>Paragraph test :: Passed</p><i class="fa-solid fa-circle-check"></i>'
+  } else {
+    document.querySelector(".paragraph-test").style.color = "red"
+  }
+
+if (document.getElementById("unnumbered")) {
+    document.querySelector(".unnumbered-test").style.color = "green"
+    document.querySelector(".unnumbered-test").innerHTML = '<p>Unnumber test :: Passed</p><i class="fa-solid fa-circle-check"></i>'
+  } else {
+    document.querySelector(".unnumbered-test").style.color = "red"
+  }
+   
+if (document.getElementById("numbered")) {
+    document.querySelector(".numbered-test").style.color = "green"
+    document.querySelector(".numbered-test").innerHTML = '<p>Number test :: Passed</p><i class="fa-solid fa-circle-check"></i>'
+  } else {
+    document.querySelector(".numbered-test").style.color = "red"
+  }
+  
+   
+if (document.getElementById("image")) {
+    document.querySelector(".image-test").style.color = "green"
+    document.querySelector(".image-test").innerHTML = '<p>Number test :: Passed</p><i class="fa-solid fa-circle-check"></i>'
+  } else {
+    document.querySelector(".image-test").style.color = "red"
+  }  
